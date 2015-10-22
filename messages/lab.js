5b5a28e5eef40eb3cf62bae75c789b39968d191b
Francesca Caiazzo <francesca.caiazzo@tufts.edu>
2015-10-21 21:38:15 -0400

    wrote parse function

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here...
+function parse() {
+
+	var request = XMLHttpRequest(); 
+	
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function () {
+		console.log("Got data back");
+	};
+
+	request.send(null);
+}
