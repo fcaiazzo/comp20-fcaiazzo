f1300b338c7236bd6a8f819c3a1e280b53cb5fa3
Francesca Caiazzo <francesca.caiazzo@tufts.edu>
2015-10-24 14:26:50 -0400

    updated lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,12 +1,20 @@
-function parse() {
+function parse () {
 
-	var request = XMLHttpRequest(); 
+	var request = new XMLHttpRequest();
 
+	//step 1
 	request.open("GET", "data.json", true);
 
+	//step 2
 	request.onreadystatechange = function() {
-		console.log("Got data back");
+		if (request.readyState == 4 && request.status == 200) {
+				console.log("got data back"); 
+				console.log(request.responseText);
+				message = JSON.parse(request.responseText);
+				elem = document.getElementById("messages");
+				elem.innerHTML = message["username"] + " : " + message["content"];
+		}		
 	};
-
+	//step 3
 	request.send(null);
 }
