91ca58c59ba6ab733fd7579cf64b25be36e3739c
Francesca Caiazzo <francesca.caiazzo@tufts.edu>
2015-10-24 14:24:32 -0400

    updated lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+function parse () {
+
+	var request = new XMLHttpRequest();
+
+	//step 1
+	request.open("GET", "data.json", true);
+
+	//step 2
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+				console.log("got data back"); 
+				console.log(request.responseText);
+				message = JSON.parse(request.responseText);
+				elem = document.getElementById("messages");
+				elem.innerHTML = message["username"] + " : " + message["content"];
+		}		
+	};
+	//step 3
+	request.send(null);
+}
